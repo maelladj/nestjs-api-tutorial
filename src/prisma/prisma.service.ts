bd16681ef933e3eb4fcfa76eff2e6688ffbc8be1
[maelladj] <[maelladj@gmail.com]>
2023-05-08 23:36:24 +0200

    NestJs config module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from 'D:/Documents/PILINK/Tutos/nestjs-api-tutorial/node_modules/.prisma/client';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient{
-    constructor() {
+    private _user: any;
+  public get user(): any {
+    return this._user;
+  }
+  public set user(value: any) {
+    this._user = value;
+  }
+    constructor(config: ConfigService) {
       super({
         datasources: {
           db: {
-            url: "postgresql://postgres:123@localhost:5434/nest?schema=public"
+            url: config.get('DATABASE_URL'),
           }  
         }
       })  
